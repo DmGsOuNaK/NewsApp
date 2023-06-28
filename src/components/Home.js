07329f58d71409edebda1a974087d1a560626fcc
DmGsOuNaK <sounak.swamiji@gmail.com>
2023-06-28 23:14:52 +0530

    added homepage

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,14 +1,40 @@
 import React from "react";
- 
+
 const Home = () => {
     return (
         <div>
-            <h1>
-                GeeksforGeeks is a Computer
-                Science portal for geeks.
-            </h1>
+            <div className="container">
+                <div className="row">
+                    <div className="col-md-6 offset-md-3">
+                        <h2 className="text-center text-dark mt-5">Kindly Log In!!</h2>
+                        <div className="text-center mb-5 text-dark">To Access The News</div>
+                        <div className="card my-5">
+
+                            <form className="card-body cardbody-color p-lg-5">
+
+                                <div className="text-center">
+                                    <img src="https://cdn.pixabay.com/photo/2016/03/31/19/56/avatar-1295397__340.png" class="img-fluid profile-image-pic img-thumbnail rounded-circle my-3" className="img-fluid profile-image-pic img-thumbnail rounded-circle my-3" width="200px" alt="profile" />
+                                </div>
+
+                                <div className="mb-3">
+                                    <input type="text" className="form-control" id="Username" aria-describedby="emailHelp" placeholder="User Name" />
+                                </div>
+                                <div className="mb-3">
+                                    <input type="password" className="form-control" id="password" placeholder="password" />
+                                </div>
+                                <div className="text-center"><button type="submit" className="btn btn-color px-5 mb-5 w-100">Login</button></div>
+                                <div id="emailHelp" className="form-text text-center mb-5 text-dark">Not
+                                    Registered? <a href="/" className="text-dark fw-bold"> Create an
+                                        Account</a>
+                                </div>
+                            </form>
+                        </div>
+
+                    </div>
+                </div>
+            </div>
         </div>
     );
 };
- 
+
 export default Home;
